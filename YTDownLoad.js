067fc3f5fb83c774289616e8e45bfe5c6f2be6da
Azure64933 <alan64933@gmail.com>
2021-10-02 16:22:45 +0800

    API direction 01

--- YTDownLoad.js
+++ YTDownLoad.js
@@ -3,6 +3,19 @@ const expressHandlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
 const multiparty = require('multiparty')
 
-const handlers = require('./lib/YT_main')
-
+const main = require('./lib/main')
 const app = express()
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+const port = process.env.PORT || 3000
+
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/views/layouts/HomePage.html');
+});
+
+app.listen(port, () => {
+    console.log(`Express started on http://localhost:${port}` +
+        '; press Ctrl-C to terminate.')
+})
