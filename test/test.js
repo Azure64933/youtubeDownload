43cd4110dce4b958184489527aa557edf7b1d173
Azure64933 <alan64933@gmail.com>
2021-10-15 13:06:54 +0800

    新增ignore

--- test/test.js
+++ test/test.js
@@ -1,3 +1,10 @@
 function callPython() {
     console.log("start call Python")
+    $.ajax({
+        type: "POST",
+        url: "/test.py",
+        data: { param: "text" }
+    }).done(function(o) {
+        console.log("done ajax")
+    });
 }
